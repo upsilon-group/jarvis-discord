1f0b9a83e50c44ec65c4c9e9000daebc547ff9ce
Luoskate <dumoulin.lucas@hotmail.fr>
2018-10-12 23:58:30 +0200

    remove subcommands for a master command

--- jarvis_discord/cogs/Random.py
+++ jarvis_discord/cogs/Random.py
@@ -12,34 +12,29 @@ class Random:
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.group(aliases=["rnd"])
-    async def random(self, ctx) -> None:
-        if ctx.invoked_subcommand is None:
-            pass
-
-    @random.command(aliases=["str", "list"])
-    async def string(self, ctx, *, values_list: str) -> None:
-        values = values_list.split()
-        result = random.choice(values)
-        await ctx.channel.send(f':game_die: **`"{result}"`**')
-
-    @random.command(aliases=["int", "number", "nbr"])
-    async def integer(
-        self, ctx, num_1: Union[int, float], num_2: Union[int, float]
-    ) -> None:
-        if isinstance(num_1 + num_2, int):
-            response = random.randint(num_1, num_2)
+    @commands.command(aliases=["rnd"])
+    async def random(self, ctx, *, msg_values: str) -> None:
+        values = []
+        for value in msg_values.split():
+            try:
+                value = float(value)
+                value = int(value) if value.is_integer() else value
+            except ValueError:
+                pass
+            finally:
+                values.append(value)
+        print(values)
+        if any(isinstance(value, str) for value in values):
+            response = random.choice(values)
+        elif any(isinstance(value, float) for value in values):
+            result = random.uniform(values[0], values[1])
+            nb_decimals = max(
+                [str(values[0])[::-1].find("."), str(values[1])[::-1].find(".")]
+            )
+            response = round(result, nb_decimals)
         else:
-            result = random.uniform(num_1, num_2)
-            nb_decimal = max([str(num_1)[::-1].find("."), str(num_2)[::-1].find(".")])
-            response = round(result, nb_decimal)
+            response = random.randint(values[0], values[1])
         await ctx.channel.send(f':game_die: **`"{response}"`**')
-
-    @random.error
-    async def random_error(self, ctx, error):
-        LOGGER.error(error)
-        if isinstance(error, commands.BadArgument):
-            await utils.self_delete(ctx, f"{' '.join(ctx.args)} est introuvable.")
 
 
 def setup(bot):
