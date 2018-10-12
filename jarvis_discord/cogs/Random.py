2b1c4dd44bfae2509f776f888802ae6f54d93588
Luoskate <dumoulin.lucas@hotmail.fr>
2018-10-13 00:04:16 +0200

    Revert "Merge branch 'master' into BitBucket/master"
    This reverts commit 139569aab02ff89a6d06734ef8d0cfbc4ad32a88.

--- jarvis_discord/cogs/Random.py
+++ jarvis_discord/cogs/Random.py
@@ -1,9 +1,11 @@
+import datetime
 import logging
 import random
 from typing import Union
 
+import discord
 from discord.ext import commands
-from jarvis_discord import utils
+from jarvis_discord import config, utils
 
 LOGGER = logging.getLogger(f"jarvis.{__name__}")
 
@@ -11,8 +13,10 @@ LOGGER = logging.getLogger(f"jarvis.{__name__}")
 class Random:
     def __init__(self, bot):
         self.bot = bot
-
-    @commands.group(aliases=["rnd"])
+        self.config = config.Config("jarvis_discord/config.json")
+        self.server = self.config.guild(bot)
+    
+    @commands.group(aliases=["rdn"])
     async def random(self, ctx) -> None:
         if ctx.invoked_subcommand is None:
             pass
@@ -21,26 +25,20 @@ class Random:
     async def string(self, ctx, *, values_list: str) -> None:
         values = values_list.split()
         result = random.choice(values)
-        await ctx.channel.send(f':game_die: **`"{result}"`**')
+        await ctx.channel.send(result)
 
     @random.command(aliases=["int", "number", "nbr"])
-    async def integer(
-        self, ctx, num_1: Union[int, float], num_2: Union[int, float]
-    ) -> None:
-        if isinstance(num_1 + num_2, int):
-            response = random.randint(num_1, num_2)
-        else:
-            result = random.uniform(num_1, num_2)
-            nb_decimal = max([str(num_1)[::-1].find("."), str(num_2)[::-1].find(".")])
-            response = round(result, nb_decimal)
-        await ctx.channel.send(f':game_die: **`"{response}"`**')
+    async def integer(self, ctx, num_1: Union[int, float], num_2: Union[int, float]) -> None:
+        result = random.uniform(num_1, num_2)
+        nb_decimal = max([str(num_1)[::-1].find('.'), str(num_2)[::-1].find('.')])
+        round_int = nb_decimal if nb_decimal != -1 else 0
+        await ctx.channel.send(round(result, round_int))
 
     @random.error
     async def random_error(self, ctx, error):
         LOGGER.error(error)
         if isinstance(error, commands.BadArgument):
             await utils.self_delete(ctx, f"{' '.join(ctx.args)} est introuvable.")
-
 
 def setup(bot):
     bot.add_cog(Random(bot))
