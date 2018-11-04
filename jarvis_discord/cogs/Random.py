bcf6f850fb2d110d32eccb66d0519b38e47b55fc
Luoskate <upsilonbot@gmail.com>
2018-11-04 14:14:02 +0100

    Merge Random rework

--- jarvis_discord/cogs/Random.py
+++ jarvis_discord/cogs/Random.py
@@ -13,32 +13,30 @@ LOGGER = logging.getLogger(f"jarvis.{__name__}")
 class Random:
     def __init__(self, bot):
         self.bot = bot
-        self.config = config.Config("jarvis_discord/config.json")
-        self.server = self.config.guild(bot)
-    
-    @commands.group(aliases=["rdn"])
-    async def random(self, ctx) -> None:
-        if ctx.invoked_subcommand is None:
-            pass
 
-    @random.command(aliases=["str", "list"])
-    async def string(self, ctx, *, values_list: str) -> None:
-        values = values_list.split()
-        result = random.choice(values)
-        await ctx.channel.send(result)
+    @commands.command(aliases=["rnd"])
+    async def random(self, ctx, *, msg_values: str) -> None:
+        values = []
+        for value in msg_values.split():
+            try:
+                value = float(value)
+                value = int(value) if value.is_integer() else value
+            except ValueError:
+                pass
+            finally:
+                values.append(value)
+        if any(isinstance(value, str) for value in values):
+            response = random.choice(values)
+        elif any(isinstance(value, float) for value in values):
+            result = random.uniform(values[0], values[1])
+            nb_decimals = max(
+                [str(values[0])[::-1].find("."), str(values[1])[::-1].find(".")]
+            )
+            response = round(result, nb_decimals)
+        else:
+            response = random.randint(values[0], values[1])
+        await ctx.channel.send(f':game_die: **`"{response}"`**')
 
-    @random.command(aliases=["int", "number", "nbr"])
-    async def integer(self, ctx, num_1: Union[int, float], num_2: Union[int, float]) -> None:
-        result = random.uniform(num_1, num_2)
-        nb_decimal = max([str(num_1)[::-1].find('.'), str(num_2)[::-1].find('.')])
-        round_int = nb_decimal if nb_decimal != -1 else 0
-        await ctx.channel.send(round(result, round_int))
-
-    @random.error
-    async def random_error(self, ctx, error):
-        LOGGER.error(error)
-        if isinstance(error, commands.BadArgument):
-            await utils.self_delete(ctx, f"{' '.join(ctx.args)} est introuvable.")
 
 def setup(bot):
     bot.add_cog(Random(bot))
